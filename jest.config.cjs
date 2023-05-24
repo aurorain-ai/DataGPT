6a98470aff91d08d395860cfe094319e08ab1af3
Michael Hu <michaelhu286@gmail.com>
2023-05-24 14:32:30 -0700

    Skip Snowflake test cases
    Skip Snowflake test cases in unit tests. Both test files can be upgraded to support both unit and integration tests using an env flag.
    Unit tests use mock and integration tests use real SDK like:
    
    const snowflakeSDK = process.env.USE_REAL_SNOWFLAKE ? require('snowflake-sdk') : require('./mocks/snowflakeMock');

--- jest.config.cjs
+++ jest.config.cjs
@@ -13,6 +13,10 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  testPathIgnorePatterns: [
+    "<rootDir>/__tests__/snowflake.test.ts",
+    "<rootDir>/__tests__/snowflakecmd.ts"
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
